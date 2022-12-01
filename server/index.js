3f46b4a35a5f977aaeb859dbf647873e7991aa6a
badouine <nzbadouine@gmail.com>
2022-12-01 04:07:25 +0000

    authRoute

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ import path from 'path';
 import authRoutes from "./routes/auth.js";
 import { register } from "./controllers/auth";
 import { fileURLToPath } from 'url';
+import { createPost } from './controllers/posts.js';
 import userRoutes from "./routes/users.js";
 import { verifyToken } from './middleware/auth.js';
 
@@ -44,11 +45,12 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
-
+app.post("/posts", postRoutes);
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL, {
