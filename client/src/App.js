6a29f0f3a6e68bf2b29087a7da9724a9fca2ff96
badouine <nzbadouine@gmail.com>
2022-12-02 02:48:57 +0000

    pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import Home from 'pages/homePage/Home';
+import Login from 'pages/loginPage/Login';
+import Profile from 'pages/profilePage/Profile';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile/:userId" element={<Profile />} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
